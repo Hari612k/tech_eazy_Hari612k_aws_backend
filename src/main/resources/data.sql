77ec8a8fc8a457938a69c921ca3a4a23376fe909
Harikrishna-Gangadi <110350777+Hari612k@users.noreply.github.com>
2025-06-13 02:46:29 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
--- Encrypted password = 'admin@123'
 INSERT INTO "user" (username, password, role) 
 VALUES ('admin', '$2a$10$U8DqM9U5gRuZcgnhpBLRwu4Yc1rN3WeP2DZzXkpz1qfJqAX2WaX7K', 'ROLE_ADMIN');
 
