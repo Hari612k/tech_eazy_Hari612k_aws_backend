81886452003a0af0029bbf6a4d077108865f6135
Harikrishna Gangadi <gangadiharikrishna314@gmail.com>
2025-06-16 15:38:42 +0530

    Save local changes before pulling

--- src/main/java/com/techeazy/zeromile/controller/ParcelSummaryController.java
+++ src/main/java/com/techeazy/zeromile/controller/ParcelSummaryController.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/summary")
 @RequiredArgsConstructor
